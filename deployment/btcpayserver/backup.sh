b3857bb34ddde75d47b67ff0984af619741f7566
Derek Smith <derek@farscapian.com>
2022-06-22 13:39:46 -0400

    Renamed/moved existing files.

---
+++ deployment/btcpayserver/backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# take the services down, create a backup archive, then pull it down.
+ssh "$FQDN" "cd $REMOTE_HOME/btcpayserver-docker/; sudo bash -c ./backup.sh"
+ssh "$FQDN" "sudo cp /var/lib/docker/volumes/backup_datadir/_data/backup.tar.gz $REMOTE_HOME/backups/btcpay.tar.gz"
+ssh "$FQDN" "sudo chown ubuntu:ubuntu $REMOTE_HOME/backups/btcpay.tar.gz"
+scp "$FQDN:$REMOTE_HOME/backups/btcpay.tar.gz" "$LOCAL_BACKUP_PATH/btcpay-$1.tar.gz"
