b23d60a6adc09ad246b24d959fae99739424bb7f
Derek Smith <derek@farscapian.com>
2022-12-26 10:34:08 -0500

    Updated publish for support git remotes.

--- publish.sh
+++ publish.sh
@@ -10,5 +10,13 @@ TAG_MESSAGE="Creating tag $TAG_NAME on $(date)."
 git tag -a "$TAG_NAME" -m "$TAG_MESSAGE" -s
 
 # optional; push to remote
-git push --all
-git push --tags
+git push --set-upstream origin --all
+git push --set-upstream origin --tags
+
+RESPONSE=
+read -r -p "         Would you like to push this to the main ss repo? (y)  ": RESPONSE
+if [ "$RESPONSE" != "y" ]; then
+    # optional; push to remote
+    git push --set-upstream ss-upstream --all
+    git push --set-upstream ss-upstream --tags
+fi
