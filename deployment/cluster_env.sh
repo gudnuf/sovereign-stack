4a84fd24e519bcb5b130ae061be25f0ed0c2b443
Derek Smith <derek@farscapian.com>
2023-03-05 11:45:27 -0500

    Refactor project env

--- deployment/cluster_env.sh
+++ deployment/cluster_env.sh
@@ -40,24 +40,3 @@ if [ ! -f "$CLUSTER_DEFINITION" ]; then
 fi
 
 source "$CLUSTER_DEFINITION"
-
-# source project defition.
-# Now let's load the project definition.
-PROJECT_NAME="$PROJECT_PREFIX-$BITCOIN_CHAIN"
-export PROJECT_NAME="$PROJECT_NAME"
-PROJECT_PATH="$PROJECTS_DIR/$PROJECT_NAME"
-PROJECT_DEFINITION_PATH="$PROJECT_PATH/project_definition"
-
-if [ ! -f "$PROJECT_DEFINITION_PATH" ]; then
-    echo "ERROR: 'project_definition' not found $PROJECT_DEFINITION_PATH not found."
-    exit 1
-fi
-
-source "$PROJECT_DEFINITION_PATH"
-export PRIMARY_SITE_DEFINITION_PATH="$SITES_PATH/$PRIMARY_DOMAIN/site_definition"
-source "$PRIMARY_SITE_DEFINITION_PATH"
-
-if [ -z "$PRIMARY_DOMAIN" ]; then
-    echo "ERROR: The PRIMARY_DOMAIN is not specified. Check your cluster definition."
-    exit 1
-fi
