cde7d287c5db52e87174a37fb3a3258f449a7f8f
Derek Smith <derek@farscapian.com>
2023-03-15 11:23:29 -0400

    Update projects commitment.

--- defaults.sh
+++ defaults.sh
@@ -101,4 +101,4 @@ export PRIMARY_DOMAIN=
 # this is the git commit of the project/ sub git repo.
 # used in the migration script to switch into past for backup
 # then back to present (TARGET_PROJECT_GIT_COMMIT) for restore.
-export TARGET_PROJECT_GIT_COMMIT=72c4df897af0a054ad938a5badf94cd13ef45ec5
+export TARGET_PROJECT_GIT_COMMIT=29acc1796dca29f56f80005f869d3bdc1faf6c58
