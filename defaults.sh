f1abfd77af3074392576e3a7ec4325b2143ce720
Derek Smith <derek@farscapian.com>
2023-03-18 18:18:21 -0400

    Project Updates.

--- defaults.sh
+++ defaults.sh
@@ -106,7 +106,7 @@ export PRIMARY_DOMAIN=
 # this is the git commit of the project/ sub git repo.
 # used in the migration script to switch into past for backup
 # then back to present (TARGET_PROJECT_GIT_COMMIT) for restore.
-export TARGET_PROJECT_GIT_COMMIT=bde59ef71718fd8810762dbbbd4877a7968a8cd1
+export TARGET_PROJECT_GIT_COMMIT=6a97c0d3f439d71be8242522d440affbe335e406
 
 # 
 export TESTNET_BLOCK_HASH=00000000d8277ba1ca66b40b3e3476629e6f0f97c5b8cfaeabfe402e55db223a
--- deployment/show.sh
+++ deployment/show.sh
@@ -5,6 +5,7 @@ echo "LXD REMOTE: $(lxc remote get-default)"
 lxc project list
 
 lxc storage list
+lxc storage volume list ss-base
 lxc image list
 lxc project list
 lxc network list
